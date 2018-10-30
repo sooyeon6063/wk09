6f72e2c4d76d3bddb2675d5448477a990d1ce58d
sooyeon6063 <cdw0033@daum.net>
2018-10-30 14:58:39 +0900

    ... changed...

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,14 +1,16 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-path = '/home/sooyeon6063/ch04'  # 해당 계정 및 폴더 확인
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-application = StaticFilesHandler(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
+application = get_wsgi_application()
